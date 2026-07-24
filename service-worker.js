9af948e19ea8815c9ffd3ce23296fdb3ed06a474
fanis-boufidis <fanisboufidis@gmail.com>
2026-07-24 21:13:33 +0300

    Update cache name to route-optimizer-v45

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'route-optimizer-v44';
+const CACHE_NAME = 'route-optimizer-v45';
 
 // Τα βασικά αρχεία που κάνουμε cache ώστε το app να ανοίγει γρήγορα / offline
 const APP_SHELL = [
